32c09e85bcfda4a5de3b230d77ac46747e2a861b
gamingAmee <amalie2410@gmail.com>
2021-11-03 09:51:18 +0100

    Mere til Mega

---
+++ Mega/Mega/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 03-11-2021 09:49:53
+ * Author : Patrick
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
